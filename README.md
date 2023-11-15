3537ce3b6476d5b691247afd67179553a2623b72
sam <jialin.hou@deeplang.ai>
2023-11-15 12:54:37 +0800

    feat: cli for create todo list crud

---
+++ README.md
@@ -0,0 +1,16 @@
+# Task Manager CLI Application
+
+## Objective
+Create a command-line interface (CLI) application to manage tasks.
+
+## Features
+* Add new tasks with a description.
+* List all tasks.
+* Update a task with taskId and description.
+* Delete a task by taskId and Delete all task.
+
+## Learning Focus:
+* Basic syntax and structures in Go (functions, loops, conditionals).
+* Working with slices and structs.
+* File I/O for saving and loading tasks.
+* Creating a simple CLI.
--- cmd/main.go
+++ cmd/main.go
@@ -25,6 +25,10 @@ func main() {
 		listTasks()
 	case "update":
 		updateTask()
+	case "delete":
+		deleteTask()
+	case "deleteAll":
+		deleteAllTask()
 	default:
 		fmt.Println("Invalid command.")
 	}
@@ -160,4 +164,88 @@ func updateTask() {
 	}
 
 	fmt.Println("Task updated successfully.")
+}
+
+// Delete a task by taskId
+func deleteTask() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter task ID: ")
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+
+	if input == "" {
+		fmt.Println("Invalid task ID.")
+		return
+	}
+
+	// Read tasks from a file
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	taskBytes, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	tasks := strings.Split(string(taskBytes), "\n")
+
+	// Update the task by ID
+	found := false
+	updatedTasks := []string{}
+	for _, task := range tasks {
+		if task != "" {
+			// split the task into id and description
+			taskParts := strings.Split(task, splitString)
+			id := strings.TrimSpace(taskParts[0])
+
+			if id == input {
+				found = true
+			} else {
+				updateTask := strings.Join(taskParts, splitString)
+				updatedTasks = append(updatedTasks, updateTask)
+			}
+		}
+	}
+
+	if !found {
+		fmt.Println("Task not found.")
+		return
+	}
+
+	tasks = updatedTasks
+
+	// Write the updated tasks to the file
+	output := strings.Join(tasks, "\n")
+	err = os.WriteFile(fileName, []byte(output), 0644)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+
+	fmt.Println("Task Remove successfully.")
+}
+
+// Delete all Task.
+func deleteAllTask() {
+	// Read tasks from a file
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// remove file
+	err = os.Remove(fileName)
+	if err != nil {
+		fmt.Println("Error remove file:", err)
+		return
+	}
+
+	fmt.Println("All Task Remove successfully.")
 }
